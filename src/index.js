1a311b88dcf282b635cd27be4475b6a8a4850c2a
benl95 <beno_1995@hotmail.com>
2021-10-27 11:31:52 +0200

    chore: add error handler

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import fetch from 'node-fetch';
-import {parseResponse} from './utils/parseResponse.js'
-import {filterKeyByReference} from './modules/filterKey.js';
+import { parseResponse } from './utils/parseResponse.js';
+import { filterKeyByReference } from './modules/filterKey.js';
 
-fetch('https://raw.githubusercontent.com/cmda-tt/course-21-22/main/tech-track-dataset.json')
+fetch(
+    'https://raw.githubusercontent.com/cmda-tt/course-21-22/main/tech-track-dataset.json'
+)
     .then(parseResponse)
-    .then(data => {
-        return data.map(obj => filterKeyByReference(obj)(
-            'Wat is je favoriete soort huisdier?',
-            'Als je later een auto zou kopen, van welk merk zou deze dan zijn?',
-            'Wat is je oogkleur?',
-            'Op een schaal van 1 tot 10, hoeveel zin heb je in de Tech Track?',
-            'Wat is je favoriete datum?',
-            'Wat is je favoriete zuivelproduct?',
-            'Welke kleur kledingstukken heb je aan vandaag? (Meerdere antwoorden mogelijk natuurlijk...)',
-            'Op welke verdieping van het TTH studeer je het liefst?',
-            'Wat wil je worden als je groot bent?',
-            'Wat wilde je later worden als je groot bent, maar nu toen je zelf 8 jaar was.'
-        ));
+    .then((data) => {
+        return data.map((obj) =>
+            filterKeyByReference(obj)(
+                'Wat is je favoriete soort huisdier?',
+                'Als je later een auto zou kopen, van welk merk zou deze dan zijn?',
+                'Wat is je oogkleur?',
+                'Op een schaal van 1 tot 10, hoeveel zin heb je in de Tech Track?',
+                'Wat is je favoriete datum?',
+                'Wat is je favoriete zuivelproduct?',
+                'Welke kleur kledingstukken heb je aan vandaag? (Meerdere antwoorden mogelijk natuurlijk...)',
+                'Op welke verdieping van het TTH studeer je het liefst?',
+                'Wat wil je worden als je groot bent?',
+                'Wat wilde je later worden als je groot bent, maar nu toen je zelf 8 jaar was.'
+            )
+        );
     })
-    .then(filtered =>  console.log(filtered))
-
-
+    .then((filtered) => console.log(filtered))
+    .catch((error) => console.log(error));
