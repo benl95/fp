54fdd1e730d829635ae0804eeabb25a734c56291
benl95 <beno_1995@hotmail.com>
2021-11-02 11:55:07 +0100

    docs: add docs for trimWhiteSpace func

--- src/modules/trimWhiteSpace.js
+++ src/modules/trimWhiteSpace.js
@@ -1,13 +1,10 @@
-/**
- * Takes a string and returns a string with no whitespaces
- * @param str target string to trim the whitespace from
- */
-// function trimWhiteSpace(str) {
-//     return str.replace(/\s+/g, '');
-// }
-
 const trimEyeColor = trimWhiteSpace('eyeColor');
 
+/**
+ * Takes a key and an object and removes the dash characters from the given key
+ * @param key target key to remove whitespace from the prop values
+ * @param obj target obj remove white space from
+ */
 function trimWhiteSpace(key) {
     return function newObj(obj) {
         const trimmed = obj[key].replace(/\s+/g, '');
